3697227fcc8641447aacedd82eb298c79be25f74
jbyomuhangi <jbyomuhangi@gmail.com>
2022-10-27 18:45:21 +0800

    Created basic post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,7 +1,39 @@
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+} from "@mui/material";
 import React from "react";
+import { MoreVert as MoreVertIcon } from "@mui/icons-material";
+
+import "./Post.css";
 
 const Post: React.FC = () => {
-  return <div>Post</div>;
+  return (
+    <Card classes={{ root: "post__card" }}>
+      <CardHeader
+        avatar={<Avatar classes={{ root: "post_avatar" }}>JB</Avatar>}
+        title="jbyomuhangi"
+        action={
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        }
+      />
+
+      <CardMedia
+        component="img"
+        height="400px"
+        src="https://images.unsplash.com/photo-1666817963926-7a87a3dcf187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+        alt="Paella dish"
+      />
+
+      <CardContent>hello there, this is the post body </CardContent>
+    </Card>
+  );
 };
 
 export default Post;
