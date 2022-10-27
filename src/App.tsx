29af114c00f8ea0c0b7a29bc2dbf1698f131beb6
jbyomuhangi <jbyomuhangi@gmail.com>
2022-10-27 14:10:45 +0800

    Installed MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
 
+import Post from "./components/Post";
+import theme from "./theme";
+
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Post />
+    </ThemeProvider>
+  );
 };
 
 export default App;
